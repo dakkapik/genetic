051f31387ae4425b87789a3ebc3abe851910d2ab
Felipe Rodas <ae86rodas@outlook.com>
2022-02-17 19:33:16 -0500

    scan fn

--- chromosome.js
+++ chromosome.js
@@ -1,8 +1,11 @@
-const matrix = require("./testArray/cleanArray.json").array
+// const matrix = require("./testArray/cleanArray.json")
+const MATRIX = require("./testArray/obstacleArray.json")
+const DEFAULT_SCAN_RADIUS = 5;
+let currentPosition = {x: 0, y: 0}
 
-// const numberedMatrix 
+MATRIX[currentPosition.y][currentPosition.x] = 'x'
 
-const scan = scanByRadius(0, 0, matrix, 1);
+const scan = scanByRadius(currentPosition.x, currentPosition.y, MATRIX, DEFAULT_SCAN_RADIUS);
 console.log(scan)
 
 
@@ -11,18 +14,28 @@ console.log(scan)
 
 
 
-function scanByRadius(x, y, matrix, radius) {
-    const result = {dirty:[], clean:[]}
-    for(let i = y - radius; i <= y + radius; i++){
-        for(let o = x - radius; o <= + radius; o++){
-            if(
-                matrix[i] 
-                // && (x !== o && y !== i)
-                ){
-                if(matrix[i][o] === '-') result.dirty.push({x: o, y: i});
-                if(matrix[i][o] === 'x') result.clean.push({x: o, y: i});
+function scanByRadius(centerX, centerY, matrix, radius) {
+    // const result = {dirty:[], clean:[]}
+    // for(let i = y - radius; i <= y + radius; i++){
+    //     for(let o = x - radius; o <= + radius; o++){
+    //         if(matrix[i]){
+    //             if(matrix[i][o] === '-') result.dirty.push({x: o, y: i});
+    //             if(matrix[i][o] === 'x') result.clean.push({x: o, y: i});
+    //         }
+    //     }
+    // }
+    // return result
+
+    const result = []
+
+    for(let x = centerX - radius; x < centerX + radius; x++){
+        console.log("x: ", x)
+        const y = Math.sqrt(Math.pow(radius, 2) - Math.pow(x, 2))
+        console.log("y: ", y)
+        if(matrix[y]){
+            if(matrix[y][x] === '-'){
+                // console.log('x: ', x, 'y: ', y)
             }
         }
     }
-    return result
 }
