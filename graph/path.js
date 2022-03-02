e2804465be02b817199e66084787a0870440cdf3
Felipe Rodas <ae86rodas@outlook.com>
2022-03-02 16:59:28 -0500

    added graph path method

--- graph/path.js
+++ graph/path.js
@@ -1,14 +1,28 @@
-let color = 1
-
 fetch("http://localhost:3000/path.json")
 .then(response =>response.json())
 .then(data => {
     data.forEach((path, index) => {
         path.forEach(position => {
             const cell = document.getElementById(`${position.x}-${position.y}`)
-            // cell.style.backgroundColor = `rgba(${color * 5}, 0, 0)`
-            cell.innerHTML = index
-            color ++;
+            cell.style.backgroundColor = `rgba(${index * 4}, 160, 160)`
+            matrix[position.y][position.x] = 'x'
+            if(position.overlap) cell.style.backgroundColor = '#ffff00'
+            if(position.cameFrom){
+                if(position.cameFrom.x === position.x){
+                    if(position.cameFrom.y > position.y){
+                        cell.innerHTML = `↑${index}`
+                    }else {
+                        cell.innerHTML = `↓${index}`
+                    }
+                } else {
+                    if(position.cameFrom.x > position.x){
+                        cell.innerHTML = `←${index}`
+                    } else {
+                        cell.innerHTML = `→${index}`
+                    }
+                }
+            }
+            // cell.innerHTML = index
         });
     });
 })
