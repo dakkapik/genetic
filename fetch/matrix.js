5de260a206c5c1e39921f3e0404307e31b57d315
Felipe Rodas <ae86rodas@outlook.com>
2022-03-12 14:31:13 -0500

    added fetch all matrix method

--- fetch/matrix.js
+++ fetch/matrix.js
@@ -1,10 +1,22 @@
+const { MATRIX_STORAGE } = require("../global_paths.json")
 const { readdirSync, readFileSync } = require("fs")
 const path = require("path")
 
+
 function fetchLastMatrix(){
-    const MATRIX_STORAGE_DIR = './public/matrixStorage';
-    const matrixAmount = readdirSync(MATRIX_STORAGE_DIR).length
-    return JSON.parse(readFileSync(path.join(MATRIX_STORAGE_DIR, `matrix_${matrixAmount}.json`)))
+    const matrixAmount = readdirSync(MATRIX_STORAGE).length
+    return JSON.parse(readFileSync(path.join(MATRIX_STORAGE, `matrix_${matrixAmount}.json`)))
 }
 
+function fetchAllMatrix() {
+    const allMatrix = readdirSync(MATRIX_STORAGE)
+    const data = []
+    allMatrix.forEach(matrix => {
+        const body = JSON.parse(readFileSync(path.join(MATRIX_STORAGE, matrix)))
+        data.push({name: matrix, body})
+    })
+    return data
+}
+
+module.exports.fetchAllMatrix = fetchAllMatrix
 module.exports.fetchLastMatrix = fetchLastMatrix;
