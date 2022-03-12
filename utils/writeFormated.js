52e40f814dbdb1c1e3fc516af70a085b055563c1
Felipe Rodas <ae86rodas@outlook.com>
2022-03-12 14:33:14 -0500

    using global paths

--- utils/writeFormated.js
+++ utils/writeFormated.js
@@ -1,14 +1,16 @@
 const { createWriteStream, mkdirSync, existsSync, readdirSync} = require("fs");
+const { MATRIX_STORAGE, PATH_STORAGE } = require("../global_paths.json")
+
 
 function matrix (matrix, dirPath = "./public") {
     return new Promise ((resolve, reject) => {
-        const MATRIX_STORAGE_DIR = dirPath + "/matrixStorage"
-        if (!existsSync(dirPath)) mkdirSync(dirPath);
-        if (!existsSync(MATRIX_STORAGE_DIR)) mkdirSync(MATRIX_STORAGE_DIR);
-    
-        const testArrayNum = readdirSync(MATRIX_STORAGE_DIR).length;
 
-        const stream = createWriteStream(MATRIX_STORAGE_DIR + `/matrix_${testArrayNum + 1}.json`);
+        if (!existsSync(dirPath)) mkdirSync(dirPath);
+        if (!existsSync(MATRIX_STORAGE)) mkdirSync(MATRIX_STORAGE);
+    
+        const testArrayNum = readdirSync(MATRIX_STORAGE).length;
+
+        const stream = createWriteStream(MATRIX_STORAGE + `/matrix_${testArrayNum + 1}.json`);
 
         stream.on("error", function (err) {reject(codify(err), "WRITE_STREAM_ERROR")})
         stream.write('[\n')
@@ -21,18 +23,17 @@ function matrix (matrix, dirPath = "./public") {
         });
         stream.write(']')
 
-        stream.end(() => resolve("Matrix written successfully..."))
+        stream.end(() => resolve("Matrix written successfully on :" + MATRIX_STORAGE + `/matrix_${testArrayNum + 1}.json`))
     })
 }
 
 function path (array, matrixName, dirPath = "./public") {
     // MAKE PATHS BETTER
     return new Promise ((resolve, reject) => {
-        const PATH_STORAGE_DIR = dirPath + "/path"
-        const PATH_MATRIX_DIR = PATH_STORAGE_DIR + "/" + matrixName
+        const PATH_MATRIX_DIR = PATH_STORAGE + "/" + matrixName
         
         if (!existsSync(dirPath)) mkdirSync(dirPath);
-        if (!existsSync(PATH_STORAGE_DIR)) mkdirSync(PATH_STORAGE_DIR);
+        if (!existsSync(PATH_STORAGE)) mkdirSync(PATH_STORAGE);
         if(!existsSync(PATH_MATRIX_DIR)) mkdirSync(PATH_MATRIX_DIR);
         
         const pathNum = readdirSync(PATH_MATRIX_DIR).length;
