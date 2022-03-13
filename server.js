f50039c2367c71fa69d2c5e737d31f5b21cc9b5b
Felipe Rodas <ae86rodas@outlook.com>
2022-03-12 21:04:01 -0500

    genetic feedback endpoint

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const aStar = require("./methods/AStar")
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { manhattamDistance } = require("./methods/heuristic")
-const writeFormated = require("./methods/writeFormated");
+const writeFormated = require("./utils/writeFormated");
+const { fetchAllMatrix } = require("./fetch/matrix")
+const genetic = require("./main")
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,21 +16,29 @@ app.post('/matrix', async (req, res) => {
     try {
         const matrix = req.body;
         const result = await writeFormated.matrix(matrix)
-        res.send(result).status(200)
+        res.send(JSON.stringify(result)).status(200)
     } catch (err){
         res.send(err).status(400)
     }
 })
 
+app.post('/genetic', async (req, res) => {
+    try {
+        const matrix = req.body
+        const history = await genetic(matrix)
+
+        res.send(JSON.stringify(history)).status(200)
+
+    } catch (err){
+        console.error(err)
+        res.send(err).status(400)
+    }
+})
+
+
 app.get('/', async (req, res) => {
-    // const matrix = req.body;
-    const matrix = require("./testArray/hardArray.json")
-    const start = {x: 0, y: 0}
-    const end = {x: matrix[0].length - 1, y: matrix.length - 1}
-
-    const result = aStar(matrix, start, end, manhattamDistance);
-
-    res.send(JSON.stringify(result));
+    const data = fetchAllMatrix()
+    res.send(JSON.stringify(data)).status(200)
 })
 
 app.listen(PORT, () => console.log("app listenig", PORT))
