2d911865f7b7c74417de1c27a1534df4b6aa5ad3
Felipe Rodas <ae86rodas@outlook.com>
2022-03-12 21:05:50 -0500

    playback global vars

--- view/methods/genetic/setup.js
+++ view/methods/genetic/setup.js
@@ -1,10 +1,11 @@
 const HOST = "http://localhost:3000/";
-const INTERVAL = 20;
 
 let playback;
-let history;
-let currentState = 0;
+let playbackSpeed;
 let matrixStorage;
+let meta;
+let history = [];
+let currentState = 0;
 let playing = false;
 
 const matrixSelect = document.getElementById("matrix-select")
@@ -13,10 +14,21 @@ const playbackButton = document.getElementById("playback-button")
 const resetButton = document.getElementById("reset-button")
 const rewindButton = document.getElementById("rewind-button")
 const forwardButton = document.getElementById("forward-button")
+const playbackSpeedSelect = document.getElementById("playback-speed-select")
+const generationDisplay = document.getElementById("generation-counter")
 
 playbackButton.innerHTML = ( playing ? "STOP" : "PLAY" )
+
+playbackSpeed = playbackSpeedSelect.value
+
+generationDisplay.innerHTML = currentState;
 
 matrixSelect.addEventListener("change", () => {
     updateMatrixDisplay(matrixStorage[matrixSelect.value].body)
     matrix = matrixStorage[matrixSelect.value].body
+})
+
+playbackSpeedSelect.addEventListener("change", () => {
+    playbackSpeed = playbackSpeedSelect.value
+    startPlayback()
 })
