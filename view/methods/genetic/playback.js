7419fc4aefc71cca4c8170508cf4bda400c34a20
Felipe Rodas <ae86rodas@outlook.com>
2022-03-12 21:05:25 -0500

    rewind and forward methods

--- view/methods/genetic/playback.js
+++ view/methods/genetic/playback.js
@@ -1,44 +1,72 @@
 resetButton.addEventListener("click", resetPlayback)
+rewindButton.addEventListener("click", rewindPlayback)
+forwardButton.addEventListener("click", forwardPlayback)
 playbackButton.addEventListener("click", ()=> {
-    playing = !playing
-    playbackButton.innerHTML = ( playing ? "STOP" : "PLAY" )
     if(playing){
+        playing = false
         stopPlayback()
     } else {
-        if(history){
+        playing = true
+        if(history != 0){
             startPlayback()
         } else {
+            console.log("FETCHING DATA...")
             getCurrentGeneticData()
-            .then(result => {
-                history = result
+            .then(({ generation: g, history: h }) => {
+                meta = g;
+                h.forEach(stage => {
+                    history.push(JSON.parse(stage))
+                });
                 startPlayback()
             })
             .catch(err => console.error("FETCH GENETIC DATA ERROR: ", err))
         } 
+        
     }
-    
+    playbackButton.innerHTML = ( playing ? "STOP" : "PLAY" )
 })
 
 function startPlayback() {
+    console.log("STARTING PLAYBACK...")
     playback = setInterval(() => {
-
         updateMatrixDisplay(history[currentState])
 
         if(currentState < history.length - 1){
             currentState ++
+            generationDisplay.innerHTML = currentState
         } else {
+            console.log("PLAYBACK ENDED...")
             clearInterval(playback);
         }
-    }, INTERVAL)
+    }, playbackSpeed)
 }
 
 function stopPlayback() {
+    console.log("STOPING PLAYBACK...")
     clearInterval(playback)
 }
 
 function resetPlayback () {
-    currentState = 0;
-    updateMatrixDisplay(history[currentState])
+    if(history != 0){
+        console.log("RESTARTING PLAYBACK...")
+        currentState = 0;
+        generationDisplay.innerHTML = currentState
+        updateMatrixDisplay(matrix)
+    }
 }
 
+function rewindPlayback () {
+    if(history != 0 && currentState - 1 > -1){
+        currentState --
+        generationDisplay.innerHTML = currentState
+        updateMatrixDisplay(history[currentState])
+    }
+}
 
+function forwardPlayback () {
+    if(history != 0 && currentState + 1 < history.length){
+        currentState ++
+        generationDisplay.innerHTML = currentState
+        updateMatrixDisplay(history[currentState])
+    }
+}
