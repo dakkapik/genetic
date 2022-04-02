4236fd92942f7082fefa26e39c5a0841046b6836
Felipe Rodas <ae86rodas@outlook.com>
2022-04-02 09:03:43 -0400

    global path

---
+++ pathing/joinPath.js
@@ -0,0 +1,15 @@
+// joins paths based on best fitness
+module.exports = function (paths) {
+    
+    const genes = new Set();
+    
+
+
+    const globalPath = [];
+
+    paths[0].forEach(cell => {
+        globalPath.push(cell)
+    });
+
+
+}
--- test.js
+++ test.js
@@ -6,6 +6,6 @@ const MUTATION_RATE = 0.2
 const POPULATION_LENGTH = 20
 
 const gen = new Population(MUTATION_RATE, POPULATION_LENGTH, MATRIX)
-const name = "gen_1.json"
+const name = "gen_2.json"
 
 writeFile("./public/gen/" + name, JSON.stringify(gen, null, 2), (err) =>{ if(err) console.error(err) })
